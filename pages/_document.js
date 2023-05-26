87dd1a2d9f67e5c573012a3cd71d359c9673bfc5
Константин <9675795@gmail.com>
2023-05-26 18:30:01 +0300

    ym

--- pages/_document.js
+++ pages/_document.js
@@ -23,35 +23,19 @@ const Document = () => {
           media="(prefers-color-scheme: dark)"
           content="#000"
         />
-        <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              <!-- Yandex.Metrika counter -->
-              <script type="text/javascript" >
-                 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                 m[i].l=1*new Date();
-                 for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                 (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-              
-                 ym(93724133, "init", {
-                      clickmap:true,
-                      trackLinks:true,
-                      accurateTrackBounce:true,
-                      webvisor:true
-                 });
-              </script>
-              <noscript><div><img src="https://mc.yandex.ru/watch/93724133" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-              <!-- /Yandex.Metrika counter -->`,
-            }}
-          />
         <noscript></noscript>
+        <YandexMetrika 
+          yid={93724133}
+          clickmap={true}
+          trackLinks={true}
+          accurateTrackBounce={true}
+          webvisor={true}
+        />        
       </Head>
       <body>
         <Main />
         <TwSizeIndicator />
         <NextScript />
-        
       </body>
     </Html>
   );
