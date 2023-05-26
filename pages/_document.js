5d9ac45f4c2f1a4ac1ea2a21e05fc0821041dae8
Константин <9675795@gmail.com>
2023-05-26 18:41:59 +0300

    ym

--- pages/_document.js
+++ pages/_document.js
@@ -23,19 +23,33 @@ const Document = () => {
           media="(prefers-color-scheme: dark)"
           content="#000"
         />
-        <noscript></noscript>
-        <YandexMetrika 
-          yid={93724133}
-          clickmap={true}
-          trackLinks={true}
-          accurateTrackBounce={true}
-          webvisor={true}
-        />        
+
       </Head>
       <body>
         <Main />
         <TwSizeIndicator />
         <NextScript />
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+                ym(93724133, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+                 });
+              `,
+            }}
+          />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/93724133" style={{ position:'absolute', left:'-9999px' }} alt="" />
+          </div>
+        </noscript>        
       </body>
     </Html>
   );
